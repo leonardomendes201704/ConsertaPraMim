ed61540fc6f11ab13603ce091324fea6ea9755a8
leonardomendes201704 <leonardomendes201704@gmail.com>
2026-02-23 08:04:31 -0300

    feat scripts de load teste

--- scripts/loadtest/run_baseline.bat
+++ scripts/loadtest/run_baseline.bat
@@ -1,5 +1,5 @@
 ﻿@echo off
 setlocal
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_loadtest.ps1" -Scenario "baseline"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0run_loadtest.ps1" -Scenario "baseline" %*
 exit /b %errorlevel%
 
